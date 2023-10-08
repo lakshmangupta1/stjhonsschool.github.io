aeaa6336a567cfd2d3236dad6383440e98a51b50
lakshmangupta1 <57612279+lakshmangupta1@users.noreply.github.com>
2023-10-08 08:02:30 +0530

    St johns School Project file
    Its a web site created by me. if any

---
+++ index.php
@@ -0,0 +1,317 @@
+<!Doctype html>
+<html>
+<head>
+<title>St Johns Digital Schools</title>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <link href="https://googleapis.com/css?family=Josefin+Sans&display=swap" rel="stylesheet">
+  </head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="index.php">St Johns Digital School</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="About.html">About</a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="contact.html">Contact Us</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+
+
+<div id="demo" class="carousel slide" data-ride="carousel">
+  <ul class="carousel-indicators">
+    <li data-target="#demo" data-slide-to="0" class="active"></li>
+    <li data-target="#demo" data-slide-to="1"></li>
+    <li data-target="#demo" data-slide-to="2"></li>
+  </ul>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/img1.jpg" alt="St Johns" width="1400" height="700">
+      <div class="carousel-caption">
+        <h3>St Johns Digital School Sonapali Nagra Ballia </h3>
+        <p>We had such a great time in Sonapali!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/img12.jpg" alt="St Johns" width="1400" height="700">
+      <div class="carousel-caption">
+        <h3>St Johns Convent School Nagra Ballia</h3>
+        <p>Thank you, Nagra!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/img19.jpg" alt="St Johns" width="1400" height="700">
+      <div class="carousel-caption">
+        <h3>Admission Open</h3>
+        <p>Dear You Have A Opportunity To Join Our Video Classes!</p>
+      </div>   
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+    <span class="carousel-control-prev-icon"></span>
+  </a>
+  <a class="carousel-control-next" href="#demo" data-slide="next">
+    <span class="carousel-control-next-icon"></span>
+  </a>
+</div>
+
+<!--About Us-->
+
+<section class= "main_heading my-5">
+	<div class="text-center py-5">
+		<h3 class="display-6">About Us</h3>
+		<hr class="w-25 mx-auto">
+	</div>		
+	<div class="container-fluid">
+		<div class="row">
+	<div class="col-lg-4 col-md-4 col-12>	
+		<div class="text-center py-5">
+		<h3 class="display-6">St Johns Digital Schools Sonapali Nagra Ballia</h3>
+		<p class="py-1"><i>Staf Name:<br>
+		<marquee behavior="alternate" onmouseover="if (!window.__cfRLUnblockHandlers) return false; this.stop();" 
+		onmouseout="if (!window.__cfRLUnblockHandlers) return false; this.start();" data-cf-modified-917d81fabc63c2dcc33c8573-="">
+			<b>Head  : Mr. Brijesh Yadav<br>
+			   Mobile   : 9919071803  ///</b>
+		</marquee>
+		<marquee behavior="alternate" onmouseover="if (!window.__cfRLUnblockHandlers) return false; this.stop();" 
+		onmouseout="if (!window.__cfRLUnblockHandlers) return false; this.start();" data-cf-modified-917d81fabc63c2dcc33c8573-="">
+			<b>Principal : Dr. Jai Shankar Gupta<br>
+			Mobile    : 9161902525  ///</b>	
+		</marquee>	</i></p>	
+		<a href="about.html" class="btn btn-success">Know More</a>
+	</div>
+	
+
+	<div class="col-lg-4 col-md-4 col-12">	
+	
+	<div id="demo" class="carousel slide" data-ride="carousel">
+		<ul class="carousel-indicators">
+		<li data-target="#demo" data-slide-to="0" class="active"></li>
+		<li data-target="#demo" data-slide-to="1"></li>
+		<li data-target="#demo" data-slide-to="2"></li>
+		</ul>
+	<div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="images/img1.jpg" alt="St Johns" width="400" height="200">
+      <div class="carousel-caption">
+        <h3>St Johns Digital School Sonapali Nagra Ballia </h3>
+        <p>We had such a great time in Sonapali!</p>
+      </div>   
+    </div>
+    <div class="carousel-item">
+      <img src="images/img10.jpg" alt="St Johns" width="400" height="200">
+      <div class="carousel-caption">
+        <h3>Admission Open</h3>
+      </div>   
+    </div>
+  
+	<div class="carousel-item">
+      <img src="images/img11.jpg" alt="St Johns" width="400" height="200">
+      <div class="carousel-caption">
+        <h3>Admission Open</h3>
+      </div>   
+    </div>
+
+	<div class="carousel-item">
+      <img src="images/img12.jpg" alt="St Johns" width="400" height="200">
+      <div class="carousel-caption">
+        <h3>Admission Open</h3>
+      </div>   
+    </div>
+
+	<div class="carousel-item">
+      <img src="images/img13.jpg" alt="St Johns" width="400" height="200">
+      <div class="carousel-caption">
+        <h3>Admission Open</h3>
+      </div>   
+    </div>
+
+	<div class="carousel-item">
+      <img src="images/img14.jpg" alt="St Johns" width="400" height="200">
+      <div class="carousel-caption">
+        <h3>Admission Open</h3>
+      </div>   
+    </div>
+
+	<div class="carousel-item">
+      <img src="images/img15.jpg" alt="St Johns" width="400" height="200">
+      <div class="carousel-caption">
+        <h3>Admission Open</h3>
+      </div>   
+    </div>
+
+	<div class="carousel-item">
+      <img src="images/img16.jpg" alt="St Johns" width="400" height="200">
+      <div class="carousel-caption">
+        <h3>Admission Open</h3>
+       </div>   
+    </div>
+
+	<div class="carousel-item">
+      <img src="images/img17.jpg" alt="St Johns" width="400" height="200">
+      <div class="carousel-caption">
+        <h3>Admission Open</h3>
+      </div>   
+    </div>
+
+	<div class="carousel-item">
+     <img src="images/img19.jpg" alt="St Johns" width="400" height="200">
+     <div class="carousel-caption">
+        <h3>Admission Open !</h3>
+	</div>   
+    </div>
+
+		<a class="carousel-control-prev" href="#demo" data-slide="prev">
+		<span class="carousel-control-prev-icon"></span>
+		</a>
+		<a class="carousel-control-next" href="#demo" data-slide="next">
+		<span class="carousel-control-next-icon"></span>
+		</a>
+	</div>
+	</div>
+</div>
+	
+			<div class="col-lg-4 col-md-4 col-12">	
+			
+				<div class="card" width="400" height="300">
+						<div class="card-body">
+							<h4 class="card-title">Our Gallary</h4>
+								<p class="card-text">St Johns Digital School Sonapali Have Lot of Memory Pictures 
+								Click Gallary btn and check our Gallary Images</p>
+								<a href="gallary.html" class="btn btn-primary">Gallary</a>
+						</div>
+				</div>
+			</div>
+	</div>
+</div>
+</section>
+
+<!--Our Services-->
+
+<section class="main_heading my-5">
+	<div class="text-center py-5">
+		<h3 class="display-4">Contact Us</h3>
+		<hr class="w-25 mx-auto">
+	</div>
+	
+	<div class="container-fluid">
+		<div class="row">
+			
+			<div class="col-lg-4 col-md-4 col-12>	
+			
+				<div class="card">
+					<div class="card" style="width:400px">
+					<img class="card-img-top" src="images/img15.jpg" alt="Card image">
+						<div class="card-body">
+							<h4 class="card-title">St John's school sonapali</h4>
+								<p class="card-text">Click Here.</p>
+									<a href="https://www.facebook.com/St-Johns-school-sonapali-1690754561230237" alt="_blank" class="btn btn-primary">St John's school sonapali</a>
+						</div>
+					</div>
+			</div>
+			
+			<div class="col-lg-4 col-md-4 col-12>	
+			
+				<div class="card">
+					<div class="card" style="width:400px">
+					<img class="card-img-top" src="images/Brijesh.jpg" alt="Card image">
+						<div class="card-body">
+							<h4 class="card-title">Mr. Brijesh Yadav</h4>
+								<p class="card-text">Click Here.</p>
+									<a href="https://www.facebook.com/brajesh.yadav.37669528" class="btn btn-primary">See Profile</a>
+						</div>
+					</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-12>	
+			
+				<div class="card">
+					<div class="card" style="width:400px">
+					<img class="card-img-top" src="images/krishnapal.jpg" alt="Card image">
+						<div class="card-body">
+							<h4 class="card-title">Mr. Krishnapal Yadav</h4>
+								<p class="card-text">click here.</p>
+									<a href="https://www.facebook.com/krishna.kp.7946281" class="btn btn-primary">See Profile</a>
+						</div>
+					</div>
+			</div>
+			
+		</div>
+	</div>
+</section>
+
+<!--Our Gallary Section-->
+
+<section class="main_heading my-5">
+	<div class="text-center py-5">
+		<h3 class="display-4">Our Gallary</h3>
+		<hr class="w-25 mx-auto">
+	</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/img10.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/img11.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/img12.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/img13.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/img14.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/img15.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/img19.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/img17.jpg" class="img-fluid pb-4">
+			</div>
+			<div class="col-lg-4 col-md-4 col-12">
+				<img src="images/img22.jpg" class="img-fluid pb-4">
+			</div>
+		</div>
+	</div>
+</section>
+
+<!--Form Page for User info-->
+
+
+<footer>
+	<p class="p-3 bg-dark text-white text-center">
+	This Web page Created By ------->> (Lakshman Gupta)<br><br>
+	<i>
+	Contact For Design Your Web Page<br>
+	Mobile- 9161197771<br>
+	Email- lakshmangupta1@gmail.com
+	</i>
+	</p>
+</footer>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
